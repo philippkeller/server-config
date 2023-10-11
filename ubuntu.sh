2979e11aaa6c9b37ed1201cee0f0840c19c2cd1e
Philipp Keller <kunigunde.knoller@pm.me>
2023-10-11 11:26:58 +0000

    LAST time, Masahiro, /dev/urandom IS NOT a variable name generator...

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
 sudo apt update
-sudo apt install -y jq build-essential postgres automake
+sudo apt install -y jq build-essential postgresql automake
 
 # tmux
-sudo apt remove tmux # version 3.2 is auto installed
+sudo apt remove tmux -y # version 3.2 is auto installed
 sudo apt install -y libevent-dev ncurses-dev build-essential bison pkg-config
 cd /var/tmp/
 wget https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz
@@ -14,11 +15,11 @@ make && sudo make install
 
 cd /var/tmp/
 git clone https://github.com/philippkeller/tmux-config.git
-./tmux-config/install.sh   
+./tmux-config/install.sh
 
 # code and environment
 cd ~
-git clone git@github.com:philippkeller/server-config.git
+git clone https://github.com/philippkeller/server-config.git
 rm -f .vimrc
 ln -s server-config/.vimrc
 rm -f .ripgreprc
@@ -29,6 +30,7 @@ rm -f .gitconfig
 ln -s server-config/.gitconfig
 rm -rf .config
 ln -s server-config/.config
+sudo apt install neovim -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/swapfiles
 source ~/.bashrc
